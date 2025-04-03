ba0c209b78c227059f17481f047f2a0940752df9
HoHarvey <63257951+HoHarvey@users.noreply.github.com>
2025-04-03 17:25:54 -0400

    Commit changes, removed NDA material from work

---
+++ app/src/main/java/sheridan/hoharv/myfirstthermalapplication/FrameDataHolder.java
@@ -0,0 +1,12 @@
+package sheridan.hoharv.myfirstthermalapplication;
+
+import android.graphics.Bitmap;
+
+class FrameDataHolder {
+
+    public final Bitmap msxBitmap;
+
+    FrameDataHolder(Bitmap msxBitmap){
+        this.msxBitmap = msxBitmap;
+    }
+}
---
+++ app/src/main/java/sheridan/hoharv/myfirstthermalapplication/MainActivity.java
@@ -0,0 +1,301 @@
+package sheridan.hoharv.myfirstthermalapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.flir.thermalsdk.ErrorCode;
+import com.flir.thermalsdk.androidsdk.ThermalSdkAndroid;
+import com.flir.thermalsdk.androidsdk.live.connectivity.UsbPermissionHandler;
+import com.flir.thermalsdk.live.CommunicationInterface;
+import com.flir.thermalsdk.live.Identity;
+import com.flir.thermalsdk.live.connectivity.ConnectionStatusListener;
+import com.flir.thermalsdk.live.discovery.DiscoveryEventListener;
+import com.flir.thermalsdk.log.ThermalLog;
+
+import java.io.IOException;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    //Handles Android permission for eg Network
+    private PermissionHandler permissionHandler;
+
+    //Handles network camera operations
+    private CameraHandler cameraHandler;
+
+    private Identity connectedIdentity = null;
+    private TextView connectionStatus;
+    private TextView discoveryStatus;
+
+    private ImageView msxImage;
+
+    private LinkedBlockingQueue<FrameDataHolder> framesBuffer = new LinkedBlockingQueue(21);
+    private UsbPermissionHandler usbPermissionHandler = new UsbPermissionHandler();
+
+    /**
+     * Show message on the screen
+     */
+    public interface ShowMessage {
+        void show(String message);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ThermalLog.LogLevel enableLoggingInDebug = BuildConfig.DEBUG ? ThermalLog.LogLevel.DEBUG : ThermalLog.LogLevel.NONE;
+
+        //ThermalSdkAndroid has to be initiated from a Activity with the Application Context to prevent leaking Context,
+        // and before ANY using any ThermalSdkAndroid functions
+        //ThermalLog will show log from the Thermal SDK in standards android log framework
+        ThermalSdkAndroid.init(getApplicationContext(), enableLoggingInDebug);
+        permissionHandler = new PermissionHandler(showMessage, MainActivity.this);
+        cameraHandler = new CameraHandler();
+        setupViews();
+    }
+
+    public void startDiscovery(View view) {
+        startDiscovery();
+    }
+    public void stopDiscovery(View view) {
+        stopDiscovery();
+    }
+    public void connectFlirOne(View view) {
+        connect(cameraHandler.getFlirOne());
+    }
+    public void disconnectFlirOne(View view) {
+        disconnect();
+    }
+
+    /**
+     * Handle Android permission request response for Bluetooth permissions
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        Log.d(TAG, "onRequestPermissionsResult() called with: requestCode = [" + requestCode + "], permissions = [" + permissions + "], grantResults = [" + grantResults + "]");
+        permissionHandler.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private void setupViews() {
+        connectionStatus = findViewById(R.id.connection_status_text);
+        discoveryStatus = findViewById(R.id.discovery_status);
+        msxImage = findViewById(R.id.msx_image);
+    }
+
+    private ShowMessage showMessage = new ShowMessage() {
+        @Override
+        public void show(String message) {
+            Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+        }
+    };
+
+
+    /**
+     * Start camera discovery
+     */
+    private void startDiscovery() {
+        cameraHandler.startDiscovery(cameraDiscoveryListener, discoveryStatusListener);
+    }
+
+    /**
+     * Stop camera discovery
+     */
+    private void stopDiscovery() {
+        cameraHandler.stopDiscovery(discoveryStatusListener);
+    }
+
+    /**
+     * Callback for discovery status, using it to update UI
+     */
+    private CameraHandler.DiscoveryStatus discoveryStatusListener = new CameraHandler.DiscoveryStatus() {
+        @Override
+        public void started() {
+            discoveryStatus.setText(getString(R.string.connection_status_text, "discovering"));
+        }
+
+        @Override
+        public void stopped() {
+            discoveryStatus.setText(getString(R.string.connection_status_text, "not discovering"));
+        }
+    };
+
+
+    /**
+     * Connect to a Camera
+     */
+    private void connect(Identity identity) {
+        //We don't have to stop a discovery but it's nice to do if we have found the camera that we are looking for
+        //cameraHandler.stopDiscovery(discoveryStatusListener);
+
+        if (connectedIdentity != null) {
+            Log.d(TAG, "connect(), in *this* code sample we only support one camera connection at the time");
+            showMessage.show("connect(), in *this* code sample we only support one camera connection at the time");
+            return;
+        }
+
+        if (identity == null) {
+            Log.d(TAG, "connect(), can't connect, no camera available");
+            showMessage.show("connect(), can't connect, no camera available");
+            return;
+        }
+
+        connectedIdentity = identity;
+
+        updateConnectionText(identity, "CONNECTING");
+        //IF your using "USB_DEVICE_ATTACHED" and "usb-device vendor-id" in the Android Manifest
+        // you don't need to request permission, see documentation for more information
+        if (UsbPermissionHandler.isFlirOne(identity)) {
+            usbPermissionHandler.requestFlirOnePermisson(identity, this, permissionListener);
+        } else {
+            doConnect(identity);
+        }
+
+    }
+
+    /**
+     * Disconnect to a camera
+     */
+    private void disconnect() {
+        updateConnectionText(connectedIdentity, "DISCONNECTING");
+        connectedIdentity = null;
+        Log.d(TAG, "disconnect() called with: connectedIdentity = [" + connectedIdentity + "]");
+        new Thread(() -> {
+            cameraHandler.disconnect();
+            runOnUiThread(() -> {
+                updateConnectionText(null, "DISCONNECTED");
+            });
+        }).start();
+    }
+
+    private UsbPermissionHandler.UsbPermissionListener permissionListener = new UsbPermissionHandler.UsbPermissionListener() {
+        @Override
+        public void permissionGranted(Identity identity) {
+            doConnect(identity);
+        }
+
+        @Override
+        public void permissionDenied(Identity identity) {
+            MainActivity.this.showMessage.show("Permission was denied for identity ");
+        }
+
+        @Override
+        public void error(UsbPermissionHandler.UsbPermissionListener.ErrorType errorType, final Identity identity) {
+            MainActivity.this.showMessage.show("Error when asking for permission for FLIR ONE, error:"+errorType+ " identity:" +identity);
+        }
+    };
+
+    private void doConnect(Identity identity) {
+        new Thread(() -> {
+            try {
+                cameraHandler.connect(identity, connectionStatusListener);
+                runOnUiThread(() -> {
+                    updateConnectionText(identity, "CONNECTED");
+                    cameraHandler.startStream(streamDataListener);
+                });
+            } catch (IOException e) {
+                runOnUiThread(() -> {
+                    Log.d(TAG, "Could not connect: " + e);
+                    updateConnectionText(identity, "DISCONNECTED");
+                });
+            }
+        }).start();
+    }
+
+    private ConnectionStatusListener connectionStatusListener = new ConnectionStatusListener() {
+        @Override
+        public void onDisconnected(@org.jetbrains.annotations.Nullable ErrorCode errorCode) {
+            Log.d(TAG, "onDisconnected errorCode:" + errorCode);
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    updateConnectionText(connectedIdentity, "DISCONNECTED");
+                }
+            });
+        }
+    };
+
+    private final CameraHandler.StreamDataListener streamDataListener = new CameraHandler.StreamDataListener() {
+
+        @Override
+        public void images(FrameDataHolder dataHolder) {
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    msxImage.setImageBitmap(dataHolder.msxBitmap);
+                }
+            });
+        }
+
+        @Override
+        public void images(Bitmap msxBitmap) {
+
+            try {
+                framesBuffer.put(new FrameDataHolder(msxBitmap));
+            } catch (InterruptedException e) {
+                //if interrupted while waiting for adding a new item in the queue
+                Log.e(TAG,"images(), unable to add incoming images to frames buffer, exception:"+e);
+            }
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Log.d(TAG,"framebuffer size:"+framesBuffer.size());
+                    FrameDataHolder poll = framesBuffer.poll();
+                    msxImage.setImageBitmap(poll.msxBitmap);
+                }
+            });
+
+        }
+    };
+
+    /**
+     * Camera Discovery thermalImageStreamListener, is notified if a new camera was found during a active discovery phase
+     * <p>
+     * Note that callbacks are received on a non-ui thread so have to eg use {@link #runOnUiThread(Runnable)} to interact view UI components
+     */
+    private DiscoveryEventListener cameraDiscoveryListener = new DiscoveryEventListener() {
+        @Override
+        public void onCameraFound(Identity identity) {
+            Log.d(TAG, "onCameraFound identity:" + identity);
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    cameraHandler.add(identity);
+                }
+            });
+        }
+
+        @Override
+        public void onDiscoveryError(CommunicationInterface communicationInterface, ErrorCode errorCode) {
+            Log.d(TAG, "onDiscoveryError communicationInterface:" + communicationInterface + " errorCode:" + errorCode);
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    stopDiscovery();
+                    MainActivity.this.showMessage.show("onDiscoveryError communicationInterface:" + communicationInterface + " errorCode:" + errorCode);
+                }
+            });
+        }
+    };
+
+    /**
+     * Update the UI text for connection status
+     */
+    private void updateConnectionText(Identity identity, String status) {
+        String deviceId = identity != null ? identity.deviceId : "";
+        connectionStatus.setText(getString(R.string.connection_status_text, deviceId +" "+ status));
+    }
+
+}
